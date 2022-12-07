f11452b717d13bd197932d519b1f783ac6bd69d9
Dani Kline <dani.kline@snyk.io>
2022-12-07 15:29:40 -0500

    added option for giturl

--- scan-repo.py
+++ scan-repo.py
@@ -4,12 +4,26 @@ import subprocess
 import re
 import json
 import typer
+import requests
 
 MANIFEST_PATTERN_SCA = '^(?![.]).*(package[.]json|Gemfile[.]lock|pom[.]xml|build[.]gradle|.*[.]lockfile|build[.]sbt|.*req.*[.]txt|Gopkg[.]lock|go[.]mod|vendor[.]json|packages[.]config|.*[.]csproj|.*[.]fsproj|.*[.]vbproj|project[.]json|project[.]assets[.]json|composer[.]lock|Podfile|Podfile[.]lock)$'
 manifest_count_dict = {
     "package.json": 0,
     "pom.xml": 0
 }
+
+def get_repos_given_org(git_org, ghtoken):
+    """
+    Uses GH API to get list of repos for get_manifest_from_clone
+    """
+    getreposurl = 'https://api.github.com/orgs/'+{git_org}+'/repos'
+    getreposheader = {'Authorization': 'token' + ghtoken }
+    ghrepos = requests.get(getreposurl, headers=getreposheader)
+    return ghrepos
+  
+
+
+
 
 def get_manifest_from_clone(repo_name, origin):
     """
@@ -96,11 +110,23 @@ def get_manifest_from_clone(repo_name, origin):
     return manifest_count_dict
 
 
-def main(giturl: str = typer.Option(..., "--gitrepo")):
-    print(f"hi {giturl}")
-    manifest_count = get_manifest_from_clone(giturl, "master")
-    serialize_manifest = json.dumps(manifest_count)
-    print (serialize_manifest)
+def main(giturl: str = typer.Option("--gitrepo", help = "If getting manifest files for a repo, enter the git url with this option"), gitorg: str = typer.Option("--gitorg", help="If getting manifest files for repos within a GitHub org, enter the name of your GitHub Organization with this option."), ghtoken: str = typer.Argument (None, envvar="GITHUB_TOKEN", help="Github Personal Access Token, required if getting manifest files withing a GitHub Org. Accepts GITHUB_TOKEN as an environmental variable.")):
+    # if --gitorg isn't entered, is gitorg undefined or is it ""
+    # Also, is this necessary? Unsure if this is suficcient or will create issues later on w/ none or both inputs
+    # gh -- optional argument with none as default variable? Optional isn't working so unsure. Also unsure how to document the env variable required--token.
+    if giturl:
+        print(f"hi {giturl}")
+        manifest_count = get_manifest_from_clone(giturl, "master")
+        serialize_manifest = json.dumps(manifest_count)
+        print (serialize_manifest)
+    if gitorg:
+        ghrepos = get_repos_given_org(gitorg,ghtoken)
+        for ghrepo in ghrepos:
+            ghrepourl = ghrepo['owner']['url']
+            manifest_count = get_manifest_from_clone(ghrepourl, "master")
+            serialize_manifest = json.dumps(manifest_count)
+            print(f"hi {ghrepourl}")
+
 
 
 if __name__ == "__main__":
